4a0e39e5d816dd1826a73179cce4e133571e1b4b
Adura Kinoshi <51264953+pkinoshi@users.noreply.github.com>
2026-05-06 01:54:44 +0100

    Add files via upload

---
+++ mappings.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 01:29:01 2026
+
+@author: ADURA
+"""
+
+# mappings.py
+
+mappings2 = {
+    'gender': {'Female': 0, 'Male': 1},
+    'SeniorCitizen': {'No': 0, 'Yes': 1},
+    'Partner': {'No': 0, 'Yes': 1},
+    'Dependents': {'No': 0, 'Yes': 1},
+    'PhoneService': {'No': 0, 'Yes': 1},
+    'MultipleLines': {'No': 0, 'No phone service': 1, 'Yes': 2},
+    'InternetService': {'DSL': 0, 'Fiber optic': 1, 'No': 2},
+    'OnlineSecurity': {'No': 0, 'No internet service': 1, 'Yes': 2},
+    'OnlineBackup': {'No': 0, 'No internet service': 1, 'Yes': 2},
+    'DeviceProtection': {'No': 0, 'No internet service': 1, 'Yes': 2},
+    'TechSupport': {'No': 0, 'No internet service': 1, 'Yes': 2},
+    'StreamingTV': {'No': 0, 'No internet service': 1, 'Yes': 2},
+    'StreamingMovies': {'No': 0, 'No internet service': 1, 'Yes': 2},
+    'Contract': {'Month-to-month': 0, 'One year': 1, 'Two year': 2},
+    'PaperlessBilling': {'No': 0, 'Yes': 1},
+    'PaymentMethod': {
+        'Bank transfer (automatic)': 0,
+        'Credit card (automatic)': 1,
+        'Electronic check': 2,
+        'Mailed check': 3
+    }
+}
+
+def apply_mappings(X):
+    X = X.copy()
+    for col, mapping in mappings2.items():
+        if col in X.columns:
+            X[col] = X[col].astype(str).replace(mapping)
+    return X
